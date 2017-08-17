45a2d5affc500e44c5147c6454a379414c0e6aa1
ErickM1992 <moreiraerick1@hotmail.com>
2017-08-17 17:38:32 -0600

    Proyecto UNED

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-contact',
@@ -7,8 +7,85 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  _curso : string = "";
+  _codigo : string = "";
+  _costo : string = "";
 
+  private cursos = [
+          {
+            nombre : "Sistemas Operativos",
+            codigo : "00526",
+            costo : "45000"
+          },
+
+          {
+            nombre : "Ingles para Informática",
+            codigo : "00516",
+            costo : "41000"
+          },
+
+          {
+            nombre : "Ingles para Informática 2",
+            codigo : "00517",
+            costo : "42000"
+          },
+
+          {
+            nombre : "Programación Intermedia",
+            codigo : "00461",
+            costo : "48000"
+          },
+
+          {
+            nombre : "Programación Avanzada",
+            codigo : "00560",
+            costo : "49000"
+          },
+
+          {
+            nombre : "Análisis de Sistemas",
+            codigo : "00520",
+            costo : "43000"
+          },
+        ]
+
+
+
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+
+
+
+  }
+
+
+  showAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Matricula Realizada!',
+      subTitle: 'Su matricula fue realizada con exito',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+
+  showAlert2() {
+    let alert = this.alertCtrl.create({
+      title: 'Matricula Cancelada con exito!',
+      subTitle: 'Su matricula fue cancelada con exito',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+
+
+
+
+  precargarCurso(curso:any ){
+      this._curso = curso.nombre;
+      this._codigo = curso.codigo;
+      this._costo = curso.costo;
   }
 
 }
